12792c85e3347d949a417a23e73ae9c5698ac9dd
Sophie Libkind <sophie.libkind@gmail.com>
2016-01-31 14:11:32 -0800

    scratch lights extension

--- lights/lights.js
+++ lights/lights.js
@@ -1,4 +1,5 @@
-
+$getScript("huejs.js")
+var bridge;
 
 (function(ext) {
 
@@ -11,10 +12,41 @@
     
     ext._getStatus = function() {
 
+        bridge = HueJS({
+            ipAddress: "10.180.30.178";
+        });
+
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.myFirstBlock = function() {
+    ext.turn_onOff = function(direction, light) {
+
+        if (direction == 'on') {
+
+            bridge.turnOn(light);
+
+        } else {
+
+            bridge.turnOff(light);
+
+        }
+    };
+
+    ext.set_color = function(light, color) {
+
+        if (color == 'white') {
+            bridge.setXY(light, 0.3227, 0.329);
+        } else if (color == 'red') {
+            bridge.setXY(light, 0.7, 0.2986);
+        } else if (color == 'blue') {
+            bridge.setXY(light, 0.139, 0.081);
+        } else if (color == 'green') {
+            bridge.setXY(light, 0.214, 0.709);
+        } else if (color == 'yellow') {
+            bridge.setXY(light, 0.4432, 0.5154);
+        } else if (color == 'purple') {
+            bridge.setXY(light, 0.2725, 0.1096);
+        }
 
     };
 
@@ -24,13 +56,17 @@
 
         blocks: [
             
-            // Block type, block name, function name
+            // Block type, block name, function name, param 1, etc.
 
-            [' ', 'my first block', 'myFirstBlock'],
+            [' ', 'turn %m.onOff light %n', 'turn_onOff', 'on', 1],
+            [' ', 'set light %n to color %m.color', 'set_color', 1, 'white'],
 
+        ],
 
-
-        ]
+        menus: {
+            onOff: ['on', 'off'],
+            color: ['white', 'red', 'blue', 'green', 'purple', 'yellow']
+        },
     };
 
 
