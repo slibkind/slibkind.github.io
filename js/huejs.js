a2ae19786ba391daa0f800d6a9334ea9115c5ab9
Sophie Libkind <sophie.libkind@gmail.com>
2016-02-01 07:39:43 -0800

    start lights.html

--- js/huejs.js
+++ js/huejs.js
@@ -1,93 +1,3 @@
-var bridge;
-
-(function(ext) {
-
-    //Cleanup function when the extension is unloaded
-    
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardward, plugin or unsupported browser
-    
-    ext._getStatus = function() {
-
-        // bridge = HueJS({
-            // ipAddress: "10.180.30.178";
-        // });
-
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.turn_onOff = function(direction, light) {
-
-        if (direction == 'on') {
-            bridge.turnOn(light);
-        } else if (direction == 'off') {
-            bridge.turnOff(light);
-        }
-    };
-
-    ext.set_color = function(light, color) {
-
-        if (color == 'white') {
-            bridge.setXY(light, 0.3227, 0.329);
-        } else if (color == 'red') {
-            bridge.setXY(light, 0.7, 0.2986);
-        } else if (color == 'blue') {
-            bridge.setXY(light, 0.139, 0.081);
-        } else if (color == 'green') {
-            bridge.setXY(light, 0.214, 0.709);
-        } else if (color == 'yellow') {
-            bridge.setXY(light, 0.4432, 0.5154);
-        } else if (color == 'purple') {
-            bridge.setXY(light, 0.2725, 0.1096);
-        }
-    };
-
-    ext.set_colorXY = function(light, x, y) {
-
-        bridge.setXY(light, x, y);
-    };
-
-            
-
-
-
-    // Block and block menu descriptions
-    
-    var descriptor = {
-
-        blocks: [
-            
-            // Block type, block name, function name, param 1, etc.
-
-            [' ', 'turn %m.onOff light %n', 'turn_onOff', 'on', 1],
-            [' ', 'set light %n to color %m.colors', 'set_color', 1, 'white'],
-            [' ', 'set light %n to color x: %n y: %n', 'set_colorXY', 1, 0.3227, 0.329],
-
-        ],
-
-        menus: {
-
-            onOff: ['on', 'off'],
-            colors: ['white', 'red', 'blue', 'green', 'purple', 'yellow']
-
-        },
-
-    };
-
-
-    // Resgister the extension
-    
-    ScratchExtensions.register('Scratch the lights', descriptor, ext);
-
-})({});
-
-// ************************************** //
-// ********** HUE JS ******************** //
-// from https://github.com/bsalinas/huejs
-// ************************************** //
-
 
 HueJS = function(params){
 	"use strict";
@@ -383,12 +293,6 @@ HueJS = function(params){
 			return setValue(lightId, {bri:bri});
 		};
 
-        setXY = function(lightId, x, y){
-            return setValue(lightId, {xy: [x, y]});
-        };
-
-        
-
 
 		return{
 			initialize:initialize,
@@ -399,8 +303,7 @@ HueJS = function(params){
 			turnOn : turnOn,
 			turnOff : turnOff,
 			setHueSat : setHueSat,
-			setBri : setBri,
-            setXY : setXY,
+			setBri : setBri
 		};
 
 };
